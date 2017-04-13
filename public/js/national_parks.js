3dadd98c44e3f4e0ff5181b18157bfd10740de97
Matt Valdez <mattvaldez01@gmail.com>
2017-04-13 12:45:44 -0500

    fixed file path to images and changed styling for h1 to h2

--- public/js/national_parks.js
+++ public/js/national_parks.js
@@ -4,52 +4,52 @@
 	$("td").click(function(){
 		switch(this.innerText) {
 			case "Acadia":
-				$("body").css("background-image", "url('/img/parks/acadia.jpg')");
-				$("h1").html(this.innerText);$("h1").css("color","honeydew");
+				$("body").css("background-image", "url('/img/acadia.jpg')");
+				$("h2").html(this.innerText);$("h2").css("color","honeydew");
 				break;
 			case "American Samoa":
-				$("body").css("background-image", "url('/img/parks/american_samoa.jpg')");
-				$("h1").html(this.innerText);$("h1").css("color","honeydew");
+				$("body").css("background-image", "url('/img/american_samoa.jpg')");
+				$("h2").html(this.innerText);$("h2").css("color","honeydew");
 				break;
 			case "Arches":
-				$("body").css("background-image", "url('/img/parks/arches.jpg')");
-				$("h1").html(this.innerText).css("color","black");
+				$("body").css("background-image", "url('/img/arches.jpg')");
+				$("h2").html(this.innerText).css("color","black");
 				break;
 			case "Badlands":
-				$("body").css("background-image", "url('/img/parks/badlands.jpg')");
-				$("h1").html(this.innerText).css("color","black");
+				$("body").css("background-image", "url('/img/badlands.jpg')");
+				$("h2").html(this.innerText).css("color","black");
 				break;
 			case "Big Bend":
-				$("body").css("background-image", "url('/img/parks/big_bend.jpg')");
-				$("h1").html(this.innerText);$("h1").css("color","honeydew");
+				$("body").css("background-image", "url('/img/big_bend.jpg')");
+				$("h2").html(this.innerText);$("h2").css("color","honeydew");
 				break;
 			case "Biscayne":
-				$("body").css("background-image", "url('/img/parks/biscayne.jpg')");
-				$("h1").html(this.innerText);$("h1").css("color","honeydew");
+				$("body").css("background-image", "url('/img/biscayne.jpg')");
+				$("h2").html(this.innerText);$("h2").css("color","honeydew");
 				break;
 			case "Black Canyon of the Gunnison":
-				$("body").css("background-image", "url('/img/parks/black_canyon_gunnison.jpg')");
-				$("h1").html(this.innerText).css("color","black");
+				$("body").css("background-image", "url('/img/black_canyon_gunnison.jpg')");
+				$("h2").html(this.innerText).css("color","black");
 				break;
 			case "Bryce Canyon":
-				$("body").css("background-image", "url('/img/parks/bryce_canyon.jpg')");
-				$("h1").html(this.innerText).css("color","black");
+				$("body").css("background-image", "url('/img/bryce_canyon.jpg')");
+				$("h2").html(this.innerText).css("color","black");
 				break;
 			case "Canyonlands":
-				$("body").css("background-image", "url('/img/parks/canyonlands.jpg')");
-				$("h1").html(this.innerText).css("color","black");
+				$("body").css("background-image", "url('/img/canyonlands.jpg')");
+				$("h2").html(this.innerText).css("color","black");
 				break;
 			case "Capitol Reef":
-				$("body").css("background-image", "url('/img/parks/capitol_reef.jpg')");
-				$("h1").html(this.innerText).css("color","black");
+				$("body").css("background-image", "url('/img/capitol_reef.jpg')");
+				$("h2").html(this.innerText).css("color","black");
 				break;      
 		}
 	});
 
 	$("td").dblclick(function(){
 		$("body").css("background-image", "none");
-		$("h1").html("National Parks");
-		$("h1").css("color","black");
+		$("h2").html("National Parks");
+		$("h2").css("color","black");
 	});
 
 })();
